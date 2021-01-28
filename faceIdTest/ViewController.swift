fd44203b02bd46665e1bf13c5a4da9e9a0638381
Igor Vedeneev <i.vedeneev@agima.ru>
2021-01-28 20:38:30 +0300

    Initial Commit

---
+++ faceIdTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  faceIdTest
+//
+//  Created by Igor Vedeneev on 28.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
